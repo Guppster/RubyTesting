f4fc2961dcc130944d3335a53c6d80c287487075
Gurpreet Singh <gurpreet@mailfence.com>
2017-09-27 19:48:37 -0400

    RPT: r001 30
    Solves step one. One challenge I ran into was that the assertion was
    failing because the recorded time was slighty larger than 120 seconds
    and i solved this by rounding the result

--- tasks/r001/count_down.rb
+++ tasks/r001/count_down.rb
@@ -1,18 +1,27 @@
 require 'minitest/autorun'
+require 'benchmark'
 
-def countdown(wait_time)
-print wait_time.to_s + " seconds left\n"
-sleep 1
-wait_time = wait_time - 1
-wait_time.downto(1).each do |time_left|
-    sleep 1
-    print time_left.to_s + " seconds left\n" if (time_left % 60) == 0
+class R001
+	def countdown(wait_time)
+		print wait_time.to_s + " seconds left\n"
+		sleep 1
+		wait_time = wait_time - 1
+		wait_time.downto(1).each do |time_left|
+			sleep 1
+			print time_left.to_s + " seconds left\n" if (time_left % 60) == 0
+		end
+		print "\a"
+	end
 end
-print "\a"
 
-end
+class R001Test < Minitest::Test
+	def test_countdown
+		rubyProgram = R001.new
 
+		time = Benchmark.measure {
+			rubyProgram.countdown(120)
+		}
 
-class CountDownTest < Minitest::Test
-
+		assert (time.real).round == 120
+	end
 end
