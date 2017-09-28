3644bfef87b9d4086e7fe1e6ebf2087aa4c6bcb8
Gurpreet Singh <gurpreet@mailfence.com>
2017-09-27 20:43:48 -0400

    RPT: r001 30
    Finished step 4 and 5 by implementing QuietPrintMethod and fixing code smells.
    Looked up how to make interfaces in Ruby because there should be one
    describing QuietPrintMethod and PrintMethod because they both
    provide print functionality but to my surprise ruby
    doesn't even support interfaces.

--- tasks/r001/count_down.rb
+++ tasks/r001/count_down.rb
@@ -1,16 +1,41 @@
 require 'minitest/autorun'
 require 'benchmark'
 
+#Supports print functionality without I/O
+class QuietPrintMethod 
+
+	#Does not print a message
+	def print(message)
+		#Throw away the message
+		@message = message
+	end
+end
+
+#Assists other classes in printing messages
+class PrintMethod 
+
+	#Prints a message
+	def print(message)
+		puts message
+	end
+end
+
+
 #Responsible for handling countdown operations
 class CountDown
 
+	#Constructor
+	def initialize(printer)
+		@printer = printer
+	end
+
 	#Waits for a specific amount of time and prints output
 	def countdown(wait_time)
-		print wait_time.to_s + " seconds left\n" 
+		@printer.print(wait_time.to_s + " seconds left\n")
 
 		wait_time.downto(1).each do |time_left|
 			sleep 1
-			print time_left.to_s + " seconds left\n" if (((time_left % 60) == 0) && time_left !=  wait_time)
+			@printer.print(time_left.to_s + " seconds left\n") if (((time_left % 60) == 0) && time_left !=  wait_time);
 		end
 	end
 end
@@ -18,7 +43,7 @@ end
 #A Test for the countdown operations class
 class CountDownTest < Minitest::Test
 	def test_120_second_countdown
-		counter = CountDown.new
+		counter = CountDown.new(QuietPrintMethod.new)
 
 		time = Benchmark.measure {
 			counter.countdown(120)
