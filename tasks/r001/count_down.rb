249f934f1448100f549b1eb24028b81d5a529308
Gurpreet Singh <gurpreet@mailfence.com>
2017-09-27 21:17:28 -0400

    RPT: r001 30
    Finished functional part of step 6. Still need to correct reek results.
    Experimented with different ways of obtaining duration in
    QuietPrintMethod and found a clean way that works. I am finding instance
    varible notation in Ruby very confusing. '@' is annoying.

--- tasks/r001/count_down.rb
+++ tasks/r001/count_down.rb
@@ -4,10 +4,23 @@ require 'benchmark'
 #Supports print functionality without I/O
 class QuietPrintMethod 
 
+	def initialize
+		@duration = 0
+		@lastTime = Time.now
+	end
+
 	#Does not print a message
 	def print(message)
+		@duration += (Time.now - @lastTime)
+
 		#Throw away the message
 		@message = message
+
+	end
+
+	#Returns the total duration from first print message to last
+	def timeInterval()
+		@duration
 	end
 end
 
@@ -37,18 +50,25 @@ class CountDown
 			sleep 1
 			@printer.print(time_left.to_s + " seconds left\n") if (((time_left % 60) == 0) && time_left !=  wait_time);
 		end
+
+		@printer.print("Finished waiting")
 	end
 end
 
 #A Test for the countdown operations class
 class CountDownTest < Minitest::Test
-	def test_120_second_countdown
-		counter = CountDown.new(QuietPrintMethod.new)
 
-		time = Benchmark.measure {
-			counter.countdown(120)
-		}
+	TIME = 5
 
-		assert (time.real).round == 120
+	def test_timed_countdown
+		printer = QuietPrintMethod.new
+
+		counter = CountDown.new(printer)
+
+		counter.countdown(TIME)
+
+		duration = quietPrinter.timeInterval
+
+		assert (duration).round == TIME
 	end
 end
