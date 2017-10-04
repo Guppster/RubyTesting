3c84eb692bc8105c415b05df96dd2a3937bf50c9
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-04 16:10:00 -0400

    RPT: r002 30
    Still have two failures from unimplemented total and sale but I have now
    created the object structures required to support the functionality they
    need. I created a Store class that contains an array of Item objects
    each with a discount struct.

--- tasks/r002/checkout.rb
+++ tasks/r002/checkout.rb
@@ -1,17 +1,44 @@
-RULES = {
-    'A' => 1
-}
+Discount = Struct.new(:quantity, :value)
+
+class Store 
+    #Initialize the items sold at this store
+    def initialize(items)
+
+        @Objects = Array.new()
+
+        for item in items
+            @Objects.push(items)
+        end
+    end
+end
+
+class Item
+    def initialize(name, price, deal)
+        @name = name
+        @price = price
+        @deal = deal
+    end
+end 
+
+RULES = Store.new([
+    Item.new("A", 50, Discount.new(3, 130)),
+    Item.new("B", 30, Discount.new(2, 45)),
+    Item.new("C", 20, Discount.new()),
+    Item.new("D", 15, Discount.new())
+])
 
 #Responsible for handling checkout operations
 class CheckOut
     def initialize(rules)
-        @rules = rules
+        @store = rules
     end
 
     def total()
     end
 
+    #Takes in a letter {A-D} and returns a numeric price
+    #Factoring in the number of times previously scanned
     def scan(item)
-        item
+
     end
 end
