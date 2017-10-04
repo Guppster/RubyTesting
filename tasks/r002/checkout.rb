a0daad1c84ba0d448e67729db04aae83c437a138
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-04 15:26:30 -0400

    RPT: r002 10
    Solved errors and achieved two failures. Need to implement total and
    scan to solve failures. Satisfied reek requirements

--- tasks/r002/checkout.rb
+++ tasks/r002/checkout.rb
@@ -2,8 +2,16 @@ RULES = {
     'A' => 1
 }
 
+#Responsible for handling checkout operations
 class CheckOut
     def initialize(rules)
         @rules = rules
+    end
+
+    def total()
+    end
+
+    def scan(item)
+        item
     end
 end
