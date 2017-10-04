f4ae19b5907b77ceadcaf3b3e3cb90a80ece1854
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-04 17:45:30 -0400

    RPT: r002 30
    Currently at 2 failures 0 errors. Figured out how to evaluate deals
    based on item and how to calculate final price. Still having issues with
    2 tests both use AAA as receipt. Its an edge case error that should be
    easy to solve

--- tasks/r002/checkout.rb
+++ tasks/r002/checkout.rb
@@ -1,12 +1,18 @@
-
 #Responsible for storing data about a single object
-Item = Struct.new(:name, :price, :deal)
+class Item < Struct.new(:name, :price, :deal)
+    def get_sale(quantity)
+        if (deal != nil)
+            return deal.evaluate(quantity)
+        end
+        return 0
+    end
+end
 
 #Represents a deal for a specific sale item
 class Discount < Struct.new(:quantity_required, :value)
-  def evaluate(number_sold)
-      return (number_sold / self.quantity_required).floor * self.value
-  end
+    def evaluate(number_sold)
+        return (number_sold / self.quantity_required).floor * self.value
+    end
 end
 
 #Represents a store and all it's functions
@@ -23,28 +29,25 @@ class Store
 
     #Given an array of items, determines the total value
     def sell_receipt(receipt)
-
-        #Categorize purchases
-        categorize(receipt);
-        
         #Calculate cost
-        receipt.inject(0) do | total_cost, item |
-            total_cost + sell_item(item)
+        receipt.inject(0) do | total_cost, (item, quantity) |
+            total_cost + sell_item(item, quantity)
         end
     end
 
     #Given a single item, determines its value
-    def sell_item(item)
-        @objects.detect{|object| object.name == item}.deal.evaluate()
-    end
+    def sell_item(item, quantity)
+        eval_object = @objects.detect{|object| object.name == item}
 
+        return quantity * eval_object.price - eval_object.get_sale(quantity)
+    end
 end
 
 RULES = Store.new([
     Item.new("A", 50, Discount.new(3, 130)),
     Item.new("B", 30, Discount.new(2, 45)),
-    Item.new("C", 20, Discount.new()),
-    Item.new("D", 15, Discount.new())
+    Item.new("C", 20),
+    Item.new("D", 15) 
 ])
 
 #Responsible for handling checkout operations
@@ -52,20 +55,16 @@ class CheckOut
 
     def initialize(rules)
         @store = rules
-        @receipt = Hash.new
+        @receipt = Hash.new(0)
     end
 
     def total()
-        return @store.sell_receipt(receipt)
+        return @store.sell_receipt(@receipt)
     end
 
     #Takes in a letter {A-D} and returns a numeric price
     #Factoring in the number of times previously scanned
     def scan(item)
-        if (@receipt[item] == nil)
-            @receipt[item] = 0
-        end
-
         @receipt[item] += 1
     end
 end
