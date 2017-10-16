857dfc72c3631b9a1dd3adb11b74a98d47415c66
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-16 23:55:27 +0000

    RPT: r003 30
    Copied over the files, and created the justify method.
    Solved the first test by using an nil check.
    9 failures remain.
    Planned out how to code the justify method

---
+++ tasks/r003/justify.rb
@@ -0,0 +1,14 @@
+
+def justify(text, max_width)
+
+    counter = 0
+
+    if (text == nil)
+        return nil
+    end
+
+    #Iterate over each word appending it's letter count to the counter
+    #When counter passes max_width param, add newline
+    #loop until end string
+
+end
