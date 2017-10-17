39a489bdab3c598ff9336e6c9d5995b76c3e4a3d
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-17 01:39:25 +0000

    RPT: r003 30
    Found a way to split words, and find the exact number of spaces needed
    in between words for the justify_line method. Currently challenged with
    placing the remainder amount of spaces into the correct positon.
    
    9 failures no errors and code smells still exist

--- tasks/r003/justify.rb
+++ tasks/r003/justify.rb
@@ -1,4 +1,5 @@
 
+#Justifies into multi lane
 def justify(text, max_width)
 
     counter = 0
@@ -11,4 +12,22 @@ def justify(text, max_width)
     #When counter passes max_width param, add newline
     #loop until end string
 
+end
+
+#Justifies just a single lane
+def justify_line(text, width)
+
+    #Extract strings
+    strings = text.split
+
+    #Get equal width of spaces 
+    spaces, remainder = (width - strings.map(&:length).inject(:+)).divmod(strings.length - 1)
+
+    #Add the number of spaces to each word
+    strings.take(strings.length - 1).each { |word| spaces.times { word << ' ' }}
+
+    #Trying to account for the remainder
+    strings.first.take(remainder).each { |word| word << ' ' }
+
+    return strings.join
 end
