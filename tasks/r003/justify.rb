705efe10ea9dc81c9ff71e207d74d72ae422437a
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-17 03:35:17 +0000

    RPT r003 30
    Solved all but one code smell. Feature envy is a major problem in this
    code problem because these are both utility functions without a class.
    The tests should be changed to make a JustificationUtility and then
    utilize it so we can make use of instance variables and avoid
    referencing self more than the parameter variables. The result of
    "fixing" the code smells to make reek happy in this problem actually
    resulted in some very badly styled programming.

--- tasks/r003/justify.rb
+++ tasks/r003/justify.rb
@@ -1,41 +1,39 @@
 
-#Justifies into multi lane
-def justify(text, max_width)
+    #Justifies into multi lane
+    def justify(text, max_width)
 
-    counter = 0
+        @rawdata = text
 
-    if (text == nil)
-        return nil
+        # Cut off a line before max_width that doesn't cut into the middle of a word
+        lines = @rawdata.match(/^.{0,#{max_width}}\b/)
+
+        # For each line thats found, use justify_line method to justify it
+        lines.each{ |line| justify_line(line, max_width) }
+
+        #Join the new lines with linebreak and return
+        return lines.join("\n")
     end
 
-    # Cut off a line before max_width that doesn't cut into the middle of a word
-    lines = text.match(/^.{0,#{max_width}}\b/)
+    #Justifies just a single lane
+    def justify_line(text, width)
 
-    # For each line thats found, use justify_line method to justify it
-    lines.each{ |line| justify_line(line, max_width) }
+        #Extract strings
+        @strings = text.split
+        @length = @strings.length
+        @real_length = @length - 1
 
-    #Join the new lines with linebreak and return
-    return lines.join("\n")
-end
+        #If only one word don't add whitespace
+        return text if @length == 1
 
-#Justifies just a single lane
-def justify_line(text, width)
+        #Get equal width of spaces 
+        spaces, remainder = (width - @strings.map(&:length).inject(:+)).divmod(@real_length)
 
-    #Extract strings
-    strings = text.split
+        #Add the number of spaces to each word
+        @strings.take(@real_length).each { |word| spaces.times { word << ' ' }}
 
-    #If only one word don't add whitespace
-    return text if strings.length == 1
+        #Takes the first string and adds the remainder number of spaces to ih
+        remainder.times{ @strings.first() << ' '}
 
-    #Get equal width of spaces 
-    spaces, remainder = (width - strings.map(&:length).inject(:+)).divmod(strings.length - 1)
-
-    #Add the number of spaces to each word
-    strings.take(strings.length - 1).each { |word| spaces.times { word << ' ' }}
-
-    #Takes the first string and adds the remainder number of spaces to ih
-    remainder.times{ strings.first() << ' '}
-
-    #Join together array of new words and return
-    return strings.join
-end
+        #Join together array of new words and return
+        return @strings.join
+    end
