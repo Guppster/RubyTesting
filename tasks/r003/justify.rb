69789b3a230a04b9edc45c607540d6b8f5e9c8c8
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-19 00:41:47 -0400

    RPT: r003 30
    Finally got reek to accept my r003 code. I learned how to split up ruby code in chunks of 3 lines
    and how to avoid feature envy by putting data into instance variables. (Even though we don't create an instance of this class).
    Currently at 5 failures 0 errors.

--- tasks/r003/justify.rb
+++ tasks/r003/justify.rb
@@ -1,39 +1,63 @@
+#Justifies into multi lane
+def justify(text, max_width)
 
-    #Justifies into multi lane
-    def justify(text, max_width)
+    #Define the data we're using
+    @rawdata = text
 
-        @rawdata = text
+    lines = justify_multiple(max_width)
 
-        # Cut off a line before max_width that doesn't cut into the middle of a word
-        lines = @rawdata.match(/^.{0,#{max_width}}\b/)
+    #Join the new lines with linebreak and return
+    return lines.join("\n")
+end
 
-        # For each line thats found, use justify_line method to justify it
-        lines.each{ |line| justify_line(line, max_width) }
+def justify_multiple(max_width)
 
-        #Join the new lines with linebreak and return
-        return lines.join("\n")
-    end
+    # Cut off a line before max_width that doesn't cut into the middle of a word
+    lines = @rawdata.match(/^.{0,#{max_width}}\b/)
 
-    #Justifies just a single lane
-    def justify_line(text, width)
+    # For each line thats found, use justify_line method to justify it
+    lines.each{ |line| justify_line(line, max_width) }
 
-        #Extract strings
-        @strings = text.split
-        @length = @strings.length
-        @real_length = @length - 1
+    return lines
+end
 
-        #If only one word don't add whitespace
-        return text if @length == 1
+#Justifies just a single lane
+def justify_line(text, width)
 
-        #Get equal width of spaces 
-        spaces, remainder = (width - @strings.map(&:length).inject(:+)).divmod(@real_length)
+    initialization(text)
 
-        #Add the number of spaces to each word
-        @strings.take(@real_length).each { |word| spaces.times { word << ' ' }}
+    #If only one word don't add whitespace
+    return text if @length == 1
 
-        #Takes the first string and adds the remainder number of spaces to ih
-        remainder.times{ @strings.first() << ' '}
+    handle_spaces(width)
 
-        #Join together array of new words and return
-        return @strings.join
-    end
+    #Join together array of new words and return
+    return @strings.join
+end
+
+def handle_spaces(width)
+    compute_space(width)
+
+    put_spaces()
+
+    #Takes the first string and adds the remainder number of spaces to ih
+    @remainder.times{ @strings.first() << ' '}
+end
+
+def put_spaces()
+    #Add the number of spaces to each word
+    @strings.take(@real_length).each { |word| @spaces.times { word << ' ' }}
+end
+
+
+def compute_space(width)
+    #Get equal width of spaces 
+    @spaces, @remainder = (width - @strings.map(&:length).inject(:+)).divmod(@real_length)
+end
+
+def initialization(text)
+    #Extract strings
+    @strings = text.split
+    @length = @strings.length
+    @real_length = @length - 1
+end
