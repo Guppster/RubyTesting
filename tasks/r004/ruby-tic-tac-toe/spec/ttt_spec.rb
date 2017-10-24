4f924ec53d2620d8d80c58c46b6f57348b1d43b2
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-24 17:30:12 -0400

    RPT: r004 30
    Learned about the expect keyword and how to use it. Reverted an edit i made to the test files in my last commit and got them to work without having to edit the require statements. Learned about the .rspec config file in the root of the project. 24 failures right now using the new expect syntax, currently learning how to do comparisons with this syntax

--- tasks/r004/ruby-tic-tac-toe/spec/ttt_spec.rb
+++ tasks/r004/ruby-tic-tac-toe/spec/ttt_spec.rb
@@ -8,61 +8,61 @@ describe Game do
 
   it "should begin with an initial score of all zeros" do
     game = Game.new
-    game.current_score.should == [[0,0,0],[0,0,0],[0,0,0]]
+    expect(game.current_score).to == [[0,0,0],[0,0,0],[0,0,0]]
   end
 
   it "should detect if user's chosen piece is on row1" do
     game = Game.new
-    game.row1_chosen?(1).should == true
+    expect(game.row1_chosen?(1)).to == true
   end
 
   it "should detect if user's chosen piece is on row2" do
     game = Game.new
-    game.row2_chosen?(5).should == true
+    expect(game.row2_chosen?(5)).to == true
   end
 
   it "should detect if user's chosen piece is on row3" do
     game = Game.new
-    game.row3_chosen?(9).should == true
+    expect(game.row3_chosen?(9)).to == true
   end
 
   it "should properly locate an input of '1'" do
     game = Game.new
-    game.row_locator(1).should == 0
+    expect(game.row_locator(1)).to == 0
   end
 
   it "should properly locate an input of '4'" do
     game = Game.new
-    game.row_locator(4).should == 1
+    expect(game.row_locator(4)).to == 1
   end
 
   it "should properly locate an input of '8'" do
     game = Game.new
-    game.row_locator(8).should == 2
+    expect(game.row_locator(8)).to == 2
   end
 
   it "should update the first row score to [1,0,0] if a piece is placed at game block 1" do
     game = Game.new
     game.place_piece(1, 1)
-    game.current_score.should == [[1,0,0],[0,0,0],[0,0,0]]
+    expect(game.current_score).to == [[1,0,0],[0,0,0],[0,0,0]]
   end
 
   it "should update the second row score to [1,0,0] if a piece is placed at game block 4" do
     game = Game.new
     game.place_piece(4, 1)
-    game.current_score.should == [[0,0,0],[1,0,0],[0,0,0]]
+    expect(game.current_score).to == [[0,0,0],[1,0,0],[0,0,0]]
   end
 
   it "should update the third row score to [0,1,0] if a piece is placed at game block 8" do
     game = Game.new
     game.place_piece(8, 1)
-    game.current_score.should == [[0,0,0],[0,0,0],[0,1,0]]
+    expect(game.current_score).to == [[0,0,0],[0,0,0],[0,1,0]]
   end
 
   it "should detect if a piece chosen would be placed on an already chosen block" do
     game = Game.new
     game.place_piece(5, 1)
-    game.already_chosen?(5).should be true
+    expect(game.already_chosen?(5)).to be true
   end
 
   it "should raise an error if a piece is placed on an already chosen block" do
@@ -75,7 +75,7 @@ describe Game do
     game = Game.new
     game.place_piece(5, 1)
     expect { game.place_piece(5, 2) }.to raise_error(RuntimeError)
-    game.current_score.should == [[0,0,0],[0,1,0],[0,0,0]]
+    expect(game.current_score).to == [[0,0,0],[0,1,0],[0,0,0]]
   end
   
 end
@@ -87,72 +87,72 @@ describe Scorer do
 
   it "should accept a score" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.score.should == [[1,1,0],[0,2,0],[2,0,2]]
+    expect(score.score).to == [[1,1,0],[0,2,0],[2,0,2]]
   end
 
   it "should label the row 1" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.row1.should == [1,1,0]
+    expect(score.row1).to == [1,1,0]
   end
 
   it "should label the row 2" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.row2.should == [0,2,0]
+    expect(score.row2).to == [0,2,0]
   end
 
   it "should label the row 3" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.row3.should == [2,0,2]
+    expect(score.row3).to == [2,0,2]
   end
 
   it "should label column 1" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.col1.should == [1,0,2]
+    expect(score.col1).to == [1,0,2]
   end
 
   it "should label column 2" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.col2.should == [1,2,0]
+    expect(score.col2).to == [1,2,0]
   end
 
   it "should label column 3" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.col3.should == [0,0,2]
+    expect(score.col3).to == [0,0,2]
   end
 
   it "should label the left diagonal" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.diag_left.should == [1,2,2]
+    expect(score.diag_left).to == [1,2,2]
   end
 
   it "should label the right diagonal" do
     score = Scorer.new([1,1,0],[0,2,0],[2,0,2])
-    score.diag_right.should == [0,2,2]
+    expect(score.diag_right).to == [0,2,2]
   end
 
   it "should pinpoint row1 as the winning location if row1 wins" do
     score = Scorer.new([1,1,1],[0,2,0],[2,0,2])
-    score.win_location.should == [1,1,1]
+    expect(score.win_location).to == [1,1,1]
   end
 
   it "should pinpoint row2 as the winning location if row2 wins" do
     score = Scorer.new([0,1,0],[2,2,2],[1,0,1])
-    score.win_location.should == [2,2,2]
+    expect(score.win_location).to == [2,2,2]
   end
 
   it "should declare player1 as the winner if player1 wins" do
     score = Scorer.new([1,1,1],[0,2,0],[2,0,2])
-    score.winner.should == "player1"
+    expect(score.winner).to == "player1"
   end
 
   it "should know if there is not a winner" do
     score = Scorer.new([0,0,1],[2,0,0],[2,0,1])
-    score.no_winner.should be true
+    expect(score.no_winner).to be true
   end
 
   it "should declare 'no winner' if no winner exists" do
     score = Scorer.new([0,0,1],[2,0,0],[2,0,1])
-    score.winner.should == "no winner"
+    expect(score.winner).to == "no winner"
   end
 
 end
