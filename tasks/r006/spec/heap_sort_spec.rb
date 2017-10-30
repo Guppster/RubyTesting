e0a3fc5fecb098499584eaa5d8b9c550eaa7d04b
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-30 19:42:30 -0400

    RPT: r006 25
    Finished Step 3 and got 100% coverage by writing two more tests. Learned
    about the sort method on arrays and used it to clean up my tests.

--- tasks/r006/spec/heap_sort_spec.rb
+++ tasks/r006/spec/heap_sort_spec.rb
@@ -5,4 +5,23 @@ describe HeapSort do
     it "should sort a simple array" do
         expect(HeapSort.new([3, 2, 1]).heap_sort).to eq [1, 2, 3]
     end
+
+    it "should sort a large array" do
+        array = [13577,
+                 29359,
+                 23121,
+                 6879,
+                 26388,
+                 21234,
+                 28665,
+                 23647,
+                 2115]
+
+        expect(HeapSort.new(array).heap_sort).to eq array.sort!
+
+    end
+
+    it "should sort a duplicat array" do
+        expect(HeapSort.new([3, 3, 2, 7, 7, 8, 9, 1]).heap_sort).to eq [1, 2, 3, 3, 7, 7, 8, 9]
+    end
 end
