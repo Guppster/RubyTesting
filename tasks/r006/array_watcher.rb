61e00c013081ac476ef48abb95aa30fcc2e28f1b
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-30 22:55:14 -0400

    RPT: r006 30
    Finished creating and testing ArrayWatcher. All tests pass and reek is
    satisfied. Had to look up attr_reader / attr_accessor again because i
    forgot about them.

--- tasks/r006/array_watcher.rb
+++ tasks/r006/array_watcher.rb
@@ -1,15 +1,25 @@
 
+# Measures the performance of an array by counting operations
 class ArrayWatcher
 
-    def initialize
+    attr_reader :counter
+
+    def initialize(array)
+        @array = array
+        @counter = 0
     end
 
     def length
+        @array.length
     end
 
     def [](index)
+        @counter += 1
+        @array[index]
     end
 
     def []=(index, value)
+        @counter += 1
+        @array[index] = value
     end
 end
--- tasks/r006/spec/array_watcher_spec.rb
+++ tasks/r006/spec/array_watcher_spec.rb
@@ -3,26 +3,37 @@ require './array_watcher.rb'
 
 describe ArrayWatcher do
     it "should initialize with an array" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        expect(watcher).to be_truthy
     end
 
     it "should initialize with a counter variable as 0" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        expect(watcher.counter).to eq 0
     end
 
     it "should return length" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        expect(watcher.length).to eq 3
     end
 
     it "should return index with []" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        expect(watcher[0]).to eq 1
     end
 
     it "should assign an index with []=" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        watcher[0] = 6
+        expect(watcher[0]).to eq 6
     end
 
     it "should increment counter each time it assigns a value to an index" do
-
+        watcher = ArrayWatcher.new([1, 2, 3])
+        watcher[0] = 6
+        watcher[2] = 5
+        watcher[1] = 4
+        watcher[0] = 8
+        expect(watcher.counter).to eq 4
     end
 end
