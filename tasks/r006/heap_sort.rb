991e7065dc05b9f8705c6e44e644295fb0eb306b
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-30 20:15:27 -0400

    RPT: r006 30
    Almost finished step 4. Fixed most reek issues, only 4 reek issues
    remain. I have improved a lot at satisfying reek, spliting stuff into
    methods has gotten easier in Ruby.

--- tasks/r006/heap_sort.rb
+++ tasks/r006/heap_sort.rb
@@ -1,52 +1,68 @@
+# Responsible for sorting arrays using the heap sort strategy 
 class HeapSort
-	attr_accessor :input
-	
-	def initialize(arg)
-		@input=arg
-	end
-	
-	def heap_sort
-	  heapify
-	  the_end=input.length-1
-	  while the_end > 0
-		input[the_end],input[0]=input[0],input[the_end]
-		the_end-=1
-		shift_down(0,the_end)
-	  end
-	  input
-	end
-	
-	def heapify
-	    the_start=(input.length-2)/2
-	     
-	     while the_start >=0 
-			shift_down(the_start,input.length-1)
-			the_start-=1
-	     end
-	end
-	
-	def shift_down(the_start,the_end)
-		root=the_start
-		while root*2+1 <= the_end
-			child=root*2+1
-			swap=root
-			
-			if input[swap] < input[child]
-				swap=child
-			end
-			
-			if child+1 <= the_end && input[swap] < input[child+1]
-				swap=child+1
-			end
-			
-			if swap!=root
-				input[root],input[swap]=input[swap],input[root]
-				root=swap
-			else
-				return
-			end
-					
-		end
-	end
-	
+    attr_reader :input
+
+    def initialize(arg)
+        @input = arg
+    end
+
+    def heap_sort
+        heapify()
+
+        the_end = input.length - 1
+
+        while the_end > 0
+            @input[the_end], @input[0] = @input[0], @input[the_end]
+            
+            the_end -= 1
+            shift_down(0, the_end)
+        end
+
+        return @input
+    end
+
+    def heapify
+        length = @input.length
+
+        the_start = (length - 2) / 2
+
+        while the_start >= 0 
+            shift_down(the_start, length - 1)
+            the_start -= 1
+        end
+    end
+
+    def shift_down(the_start, the_end)
+
+        while the_start * 2 + 1 <= the_end
+            child = the_start * 2 + 1
+
+            swap = prepare_swap(the_start, child)
+
+            return unless swap != the_start
+
+            perform_swap(the_start, swap)
+
+            the_start = swap
+        end
+    end
+
+    def prepare_swap(swap, child)
+            next_child = child + 1
+
+            if @input[swap] < @input[child]
+                swap = child
+            end
+
+            if next_child <= the_rnd && @input[swap] < @input[next_child]
+                swap = next_child
+            end
+            
+            return swap
+    end
+
+    def perform_swap(the_start, swap)
+            @input[the_start], @input[swap] = @input[swap], @input[the_start]
+    end
+
 end
