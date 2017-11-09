fd3dce6919dd701af35c91b7bcb1266f7ebbc5fa
Gurpreet Singh <gurpreet@mailfence.com>
2017-11-08 21:27:43 -0500

    RPT: r007 30
    Step 2 Finished. Got coverage to 97.6%, only one line remains in which I
    have decided to ask for assistance because after lots of attempts at
    understanding the logic in the choose_pivot method i was unable to cover
    that line with many new test cases introduced. Therefore, I will move on
    to step 3 until i can get assistance on that topic

--- tasks/r007/spec/quicksort_median_of_three_spec.rb
+++ tasks/r007/spec/quicksort_median_of_three_spec.rb
@@ -12,6 +12,15 @@ describe Sort do
     end
   end
 
+  describe  "choose_pivot" do
+    let(:input) {[1,-6,-1, -1, -5]}
+    let(:output) {subject.choose_pivot(input, 1, input.length - 1)}
+
+    it "choose pivot element" do
+      expect(output).to eq 4
+    end
+  end
+
   describe("partition") do
     let(:input) { [3, 6, 2, 3, 9, 7] }
     let(:output) { subject.partition(input, 0, input.length - 1) }
@@ -30,12 +39,22 @@ describe Sort do
     end
   end
 
-  describe  "quicksot" do
+  describe  "quicksort" do
     let(:input) {[3,6,2,3,9,7]}
     let(:output) {subject.quicksort(input, 0, input.length - 1)}
 
     it "sort of array" do
       expect(output).to eq [2,3,3,6,7,9]
+    end
+  end
+
+  describe  "quicksort" do
+    let(:input) {[3,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,7]}
+    let(:output) {subject.quicksort(input, 0, input.length - 1)}
+
+    it "sort of array" do
+      expect(output).to eq [2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 6, 7, 9, 9, 9, 9, 9, 9, 9, 9]
+
     end
   end
 end
