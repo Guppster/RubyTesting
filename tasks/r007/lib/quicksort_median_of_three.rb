e1a93eb65b2ad1cd99dd05bf55938e82f84b014a
Gurpreet Singh <gurpreet@mailfence.com>
2017-11-15 17:18:13 -0500

    RPT: r007 30
    Continuing the process of eliminating mutations. Have gotten them down
    to 28 mutations and 92.7 percent coverage in the mutation test by
    writing some new tests to cover edge cases and optimizing the sort code
    to have less mutatable senarios.

--- tasks/r007/lib/quicksort_median_of_three.rb
+++ tasks/r007/lib/quicksort_median_of_three.rb
@@ -3,7 +3,7 @@ require "quicksort_median_of_three/version"
 class Sort
 
   def self.quicksort(ar, lo, hi)
-    while lo < hi
+    while lo <= hi
       p = partition(ar, lo, hi)
       quicksort(ar, lo, p - 1)
       lo = p + 1
@@ -14,14 +14,14 @@ class Sort
 
   def self.partition(ar, lo, hi)
     index = choose_pivot(ar, lo, hi)
-    pivot = ar[index]
+    pivot = ar.fetch(index)
 
     ar[index], ar[hi] = ar[hi], ar[index]
 
     i = lo - 1
 
     (lo...hi).each do |j|
-      if ar[j] <= pivot
+      if ar.fetch(j) <= pivot
         i += 1
         ar[i], ar[j] = ar[j], ar[i]
       end
@@ -41,20 +41,5 @@ class Sort
     return right if (ar.fetch(right) - ar.fetch(left)) * (ar.fetch(center) - ar.fetch(right)) >= 0
 
     center
-  end
-
-  def self.insertion_sort(ar, lo, hi)
-    (lo + 1).upto(hi) do |i|
-      value = ar.fetch(i)
-      j = i - 1
-
-      while j >= 0 && ar[j] > value
-        ar[j + 1] = ar.fetch(j)
-        j -= 1
-      end
-
-      ar[j + 1] = value
-    end
-    return ar
   end
 end
--- tasks/r007/spec/quicksort_median_of_three_spec.rb
+++ tasks/r007/spec/quicksort_median_of_three_spec.rb
@@ -30,16 +30,7 @@ describe Sort do
     end
   end
 
-  describe("insersorting") do
-    let(:input) { [3, 6, 2, 3, 9, 7] }
-    let(:output) { subject.insertion_sort(input, 0, input.length - 1) }
-
-    it "sort" do
-      expect(output).to eq [2,3,3,6,7,9]
-    end
-  end
-
-  describe  "quicksort" do
+  describe 'quicksort' do
     let(:input) {[3,6,2,3,9,7]}
     let(:output) {subject.quicksort(input, 0, input.length - 1)}
 
@@ -48,19 +39,43 @@ describe Sort do
     end
   end
 
-  describe  "quicksort center pivot" do
+  describe 'quicksort negative' do
     let(:input) {[1,-6,-1, -1, -5]}
-    let(:output) { subject.quicksort(input, 5, input.length - 1) }
+    let(:output) { subject.quicksort(input, 0, input.length - 1) }
 
-    it "sort of array" do
+    it 'sort of array' do
     end
   end
 
-  describe  "quicksortbig" do
+  describe 'quicksort same high and low' do
+    let(:input) {[1,-6,-1, -1, 1]}
+    let(:output) { subject.quicksort(input, 0, input.length - 1) }
+
+    it 'sort of array' do
+    end
+  end
+
+  describe 'quicksort small' do
+    let(:input) {[1,2]}
+    let(:output) { subject.quicksort(input, 5, input.length - 1) }
+
+    it 'sort of array' do
+    end
+  end
+
+  describe 'quicksort small reverse' do
+    let(:input) {[2,1]}
+    let(:output) { subject.quicksort(input, 5, input.length - 1) }
+
+    it 'sort of array' do
+    end
+  end
+
+  describe 'quicksort big' do
     let(:input) {[3,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,6,2,3,9,7]}
     let(:output) {subject.quicksort(input, 0, input.length - 1)}
 
-    it "sort of array" do
+    it 'sort of array' do
       expect(output).to eq [2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 6, 7, 9, 9, 9, 9, 9, 9, 9, 9]
 
     end
