c7d5b8de0dcb3848ef83e0a073ee608c2d9a9cb0
Gurpreet Singh <gurpreet@mailfence.com>
2017-12-04 13:42:22 -0500

    RPT: r011 30
    More progress on step 3 of r011 checklist. Finished determining how
    many one_offs and right_ons each player has. Checked for reek issues and
    fixed them. Wrote tests for all methods I wrote.

--- tasks/r011/beancounting.rb
+++ tasks/r011/beancounting.rb
@@ -1,10 +1,16 @@
+
+Player = Struct.new(:one_off, :right_on)
+
 # Handles bean counting operations
 class BeanCounting
+  attr_reader :players
+
   def initialize(beans)
     @raw_beans = beans
     @total_guesses = 0
     @total_players = 0
     @players = {}
+    process_raw_beans
   end
 
   def count_beans(type)
@@ -26,8 +32,17 @@ class BeanCounting
     @guess_array.each do |guess|
       current_guess = guess.split(' ')
 
+      @players[current_guess[0]] = Player.new(0, 0)
 
+      current_guess[1].split(',').each do |entry|
+        entry_array = entry.split(':')
 
+        if @beans[entry_array[0]] + 1 == entry_array[1] || @beans[entry_array[0]] - 1 == entry_array[1]
+          @players[current_guess[0]].one_off += 1
+        elsif @beans[entry_array[0]] == entry_array[1]
+          @players[current_guess[0]].right_on += 1
+        end
+      end
     end
   end
 
--- tasks/r011/spec/beancounting_spec.rb
+++ tasks/r011/spec/beancounting_spec.rb
@@ -14,4 +14,15 @@ describe BeanCounting do
 
     expect(@contest.process_raw_beans).to eq result
   end
+
+  it 'returns correct players hash after submitting guesses' do
+    @contest = BeanCounting.new('abcda')
+
+    result = { "A" => Player.new(1, 2), "B" => Player.new(1, 0), "C" => Player.new(0, 1) }
+
+    guesses = "A a:2,c:2\nB a:1\nC d:1\n"
+    @contest.submit_guesses(guesses)
+
+    expect(@contest.players).to eq result
+  end
 end
