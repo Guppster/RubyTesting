8a2dbfc0cc4f79bac83400901a726c623c26222b
Gurpreet Singh <gurpreet@mailfence.com>
2017-12-04 12:39:55 -0500

    RPT: r011 30
    Progress on Step 3 of checklist for r011. Moved further in
    implementation. Now i know how many of each bean there are in my
    program and can accept guesses. All that remains is result processing.

--- tasks/r011/beancounting.rb
+++ tasks/r011/beancounting.rb
@@ -1,16 +1,38 @@
+# Handles bean counting operations
 class BeanCounting
   def initialize(beans)
-    @rawBeans = beans
+    @raw_beans = beans
+    @total_guesses = 0
+    @total_players = 0
+    @players = {}
   end
 
   def count_beans(type)
-    @rawBeans.count(type)
+    @raw_beans.count(type)
+  end
+
+  def process_raw_beans
+    @beans =
+      Hash[@raw_beans
+      .split('')
+      .group_by { |char| char }
+      .map { |char, count| [char, count.size] }
+      .sort_by { |char, count| [-count, char] }]
   end
 
   def submit_guesses(guesses)
+    @guess_array = guesses.split("\n")
+
+    @guess_array.each do |guess|
+      current_guess = guess.split(' ')
+
+
+
+    end
   end
 
   def get_reward(player)
+
   end
 
   def get_reward_list
--- tasks/r011/spec/beancounting_spec.rb
+++ tasks/r011/spec/beancounting_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe BeanCounting do
-
   it 'count beans returns correct amount after initialization' do
     @contest = BeanCounting.new('abcda')
 
     expect(@contest.count_beans('a')).to eq 2
   end
 
+  it 'raw beans are processed correctly' do
+    @contest = BeanCounting.new('abcda')
+
+    result = { "a" => 2, "b" => 1, "c" => 1, "d" => 1 }
+
+    expect(@contest.process_raw_beans).to eq result
+  end
 end
